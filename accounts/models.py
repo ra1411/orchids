5b8008f7449966e916c14592398936dfe1b96896
raja4752 <Raja@DESKTOP-RSONJFH.(none)>
2021-09-18 19:03:26 +0530

    update

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,10 @@ from django.db import models
 # Create your models here.
 from django.dispatch import receiver
 from django.urls import reverse
+<<<<<<< HEAD
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
 # Create your models here.
+=======
+from django.core.mail import send_mail  
+>>>>>>> 0583b9a8676ef469190a2fe397ccc0aa28fec33b
