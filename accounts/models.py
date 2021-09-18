9f443172662478855cd2a472eb127ceb928f3849
ra1411 <90439601+ra1411@users.noreply.github.com>
2021-09-18 19:39:52 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,23 @@
+models.py
+"""
 from django.db import models
-from django.db import models
+from django.contrib.auth.signals import user_logged_in, user_logged_out  
 
-# Create your models here.
-from django.dispatch import receiver
-from django.urls import reverse
-<<<<<<< HEAD
-from django_rest_passwordreset.signals import reset_password_token_created
-from django.core.mail import send_mail
-# Create your models here.
-=======
-from django.core.mail import send_mail  
->>>>>>> 0583b9a8676ef469190a2fe397ccc0aa28fec33b
+class LoggedUser(models.Model):
+  username = models.CharField(max_length=30, primary_key=True)
+  
+  def __unicode__(self):
+    return self.username
+
+def login_user(sender, request, user, **kwargs):
+  LoggedUser(username=user.username).save()
+
+def logout_user(sender, request, user, **kwargs):
+  try:
+    u = LoggedUser.objects.get(pk=user.username)
+    u.delete()
+  except LoggedUser.DoesNotExist:
+    pass
+    
+user_logged_in.connect(login_user)
+user_logged_out.connect(logout_user)
